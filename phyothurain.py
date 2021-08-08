83d5cf43dc98f1aa42223a441a86d94bb12db19b
Hacker Min <88605415+phyothurain295646@users.noreply.github.com>
2021-08-08 14:56:24 +0630

    phyothurain.py
      pkg update && upgrade
    
     pkg install git
    
     pkg install python2
    
     pip2 install requests
    
     pip2 install mechanize
    
     pkg install local
    
     pip2 install bs4
    
    git clone https://github.com/phyothurain295646/Phyo-Thu-Rain.git
    
    cd phyothurain
    
     python3 phyothurain.py

---
+++ phyothurain.py
@@ -0,0 +1,20 @@
+  pkg update && upgrade
+
+ pkg install git
+
+ pkg install python2
+
+ pip2 install requests
+
+ pip2 install mechanize
+
+ pkg install local
+
+ pip2 install bs4
+
+git clone https://github.com/phyothurain295646/Phyo-Thu-Rain.git
+
+cd phyothurain
+
+ python3 phyothurain.py
+
